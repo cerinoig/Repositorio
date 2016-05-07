9a108f86f2bf2ddd0b3405ede5bc4d806c48bc36
Ivan <Cocacola123>
2016-05-06 21:50:05 -0300

    Creacion de layout Recetas

--- app/src/main/java/com/example/laboratorioiii/myapplication/Recetas.java
+++ app/src/main/java/com/example/laboratorioiii/myapplication/Recetas.java
@@ -3,7 +3,11 @@ package com.example.laboratorioiii.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class Recetas extends AppCompatActivity {
+    private  String Nombre;
+    private  String Subtitulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
