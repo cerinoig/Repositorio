48c176f76849c48fdffd0d99eeac8d116764847e
Ivan <Cocacola123>
2016-05-06 21:23:26 -0300

    Creacion de layout principal

--- app/src/main/java/com/example/laboratorioiii/myapplication/MainActivity.java
+++ app/src/main/java/com/example/laboratorioiii/myapplication/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.laboratorioiii.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout recetas;
+    private LinearLayout quecocino;
+    private LinearLayout compartirReceta;
+    private LinearLayout info;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recetas = (LinearLayout) findViewById(R.id.btn1);
+        quecocino = (LinearLayout) findViewById(R.id.btn2);
+        compartirReceta = (LinearLayout) findViewById(R.id.btn3);
+        info = (LinearLayout) findViewById(R.id.btn4);
+
+        recetas.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(v.getContext(), Recetas.class);
+                startActivity(intent);
+            }
+
+        });
+
+        quecocino.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+
+                intent.setClass(v.getContext(), Recetas.class);
+                startActivity(intent);
+            }
+
+        });
+
+        compartirReceta.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+
+                intent.setClass(v.getContext(), Recetas.class);
+                startActivity(intent);
+            }
+
+
+        });
     }
 }
---
+++ app/src/main/java/com/example/laboratorioiii/myapplication/Recetas.java
@@ -0,0 +1,7 @@
+package com.example.laboratorioiii.myapplication;
+
+/**
+ * Created by Ivan on 6/5/2016.
+ */
+public class Recetas {
+}
