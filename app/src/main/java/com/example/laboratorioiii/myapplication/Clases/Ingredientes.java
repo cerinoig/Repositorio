83761f8663b7087d2e893244ba4779ae338a5615
Ivan <Cocacola123>
2016-05-20 21:44:36 -0300

    Creacion de layout Recetas

---
+++ app/src/main/java/com/example/laboratorioiii/myapplication/Clases/Ingredientes.java
@@ -0,0 +1,17 @@
+package com.example.laboratorioiii.myapplication.Clases;
+
+/**
+ * Created by Ivan on 20/5/2016.
+ */
+public class Ingredientes {
+
+    private String Descripcion;
+    private int cantidad;
+    
+
+}
+
+
+
+
+
---
+++ app/src/main/java/com/example/laboratorioiii/myapplication/Clases/Paso.java
@@ -0,0 +1,20 @@
+package com.example.laboratorioiii.myapplication.Clases;
+
+/**
+ * Created by Ivan on 20/5/2016.
+ */
+public class Paso {
+
+    private  String descripcion;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+
+
+}
