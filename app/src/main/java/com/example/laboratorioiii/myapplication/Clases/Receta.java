2a09dd70c160b6a85adfe5c82f536656364d488d
Ivan <Cocacola123>
2016-05-27 19:43:22 -0300

    Creacion de layout Recetas

---
+++ app/src/main/java/com/example/laboratorioiii/myapplication/Clases/Receta.java
@@ -0,0 +1,49 @@
+package com.example.laboratorioiii.myapplication.Clases;
+
+import java.util.List;
+
+/**
+ * Created by Ivan on 27/5/2016.
+ */
+public class Receta {
+
+    private String titulo;
+
+    private List<String> ingredientes;
+
+    private List<String> pasos;
+
+    private String autor;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public List<String> getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(List<String> ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public List<String> getPasos() {
+        return pasos;
+    }
+
+    public void setPasos(List<String> pasos) {
+        this.pasos = pasos;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+}
